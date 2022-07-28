0e92d8cf1f9eddf01ccdb125557fd207026d1cfd
sdineen <dineen701@gmail.com>
2022-07-28 13:39:23 +0100

    FizzBuzz 2

--- src/com/qa/example/FizzBuzz.java
+++ src/com/qa/example/FizzBuzz.java
@@ -4,23 +4,20 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String value = getResponse(16);
+		String value = getResponse(15);
 		System.out.println(value);
 	}
 
 	private static String getResponse(int value) {
 		String text = "";
-		if (!(value % 3 == 0 || value % 5 == 0)) {
-			return Integer.toString(value);
-		}
 		if (value % 3 == 0) {
 			text = "Fizz";
 		}
 		if (value % 5 == 0) {
-			text = "Buzz";
+			text += "Buzz";
 		}
-		if (value % 5 == 0 && value % 3 == 0) {
-			text = "FizzBuzz";
+		if (text.isBlank()) {
+			return Integer.toString(value);
 		}
 		return text;
 
