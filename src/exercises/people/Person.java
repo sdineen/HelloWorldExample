cfeb63d76b638c05933c4dcd77d4d5050e8ef317
sdineen <dineen701@gmail.com>
2022-08-01 13:09:31 +0100

    m

--- src/exercises/people/Person.java
+++ src/exercises/people/Person.java
@@ -1,5 +1,44 @@
 package exercises.people;
 
 public class Person {
+	//instance variables
+	private String name;
+	private int age;
+	private String jobTitle;
+	
+	
+	
+	public Person(String name, int age, String jobTitle) {
+		this.name = name;
+		this.age = age;
+		this.jobTitle = jobTitle;
+	}
+
+	//instance methods
+	
+	@Override
+	public String toString() {
+		return name + " age: " + age + " job title "+jobTitle;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
 
 }
--- src/exercises/people/Runner.java
+++ src/exercises/people/Runner.java
@@ -1,10 +1,42 @@
 package exercises.people;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
+		Person p1 = new Person("Dave", 32, "accountant");
+		Person p2 = new Person("Sue", 35, "solicitor");
+		
+		List<Person> people = new ArrayList<>();
+		people.add(p1);
+		people.add(p2);
+		
+		for (Person person : people) {
+			System.out.println(person);
+		}
+		
+		
+		
+//		Scanner scanner1 = new Scanner(System.in);
+//		System.out.println("Enter name:");
+//		String name = scanner1.nextLine();
+//		System.out.println("Enter age");
+//		int age = Integer.parseInt(scanner1.nextLine());
+//		System.out.println("Enter job title");
+//		String jobTitle = scanner1.nextLine();
+//		
+//		Person person1 = new Person(name, age, jobTitle);
+////		person1.setName(name);
+////		person1.setAge(age);
+////		person1.setJobTitle(jobTitle);
+//		
+//		System.out.println(person1);
+//	}
 	}
 
 }
